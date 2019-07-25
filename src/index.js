3c0aef54d7daad11e465f5061999a2331df7f375
yoyomade <yoyomade@google.com>
2019-07-25 15:47:43 -0400

    changes to JSON body and function comments

--- src/index.js
+++ src/index.js
@@ -3,14 +3,24 @@ const { google } = require('googleapis');
 const readline = require('readline');
 var oauth2Client = new google.auth.OAuth2();
 
+
+/**
+ * HTTP Cloud Function that obtains image file ID &
+ * authentication from client & makes calls to 
+ * helper functions.
+ *
+ * @param {Object} req Cloud Function request context.
+ * @param {Object} res Cloud Function response context.
+ */
 exports.analyzeImage = async (req, res) => {
   handleCors(req, res);
-  // Get variables from client.
+  
+  // Obtains 'POST' request data.
   var inputImageUrl = req.body.fileId;
   var authToken = req.body.auth;
- 
+  
   getLabels(inputImageUrl, authToken).then(url => {
-    console.log("Returning this Sheet URL to client: " + url);
+    console.log("Returning URL to client: " + url); 
     res.status(200).type('text/json')
       .end(JSON.stringify(url));
   }).catch(err => {
@@ -19,11 +29,15 @@ exports.analyzeImage = async (req, res) => {
   })
 };
 
-// Create column chart.
+/**
+ * Creates a bar chart with given Sheet's data.
+ *
+ * @param {String} spreadsheetId ID of sheet to access.
+ * @param {String} authToken Authentication to access user's sheet.
+ */
 function createChart(spreadsheetId, authToken) {
   console.log("Inside createChart()");
   
-  // Refresh auth token.
   oauth2Client.setCredentials({
    access_token: authToken
   });
@@ -31,18 +45,8 @@ function createChart(spreadsheetId, authToken) {
                               auth: oauth2Client
                              });  
   
-  // JSON request body.
-  var chartRequest = { 
-   requests: [
-    {
-      addChart: {
-        chart: {
-          spec: {
-            title: "Vision API Data",
-            basicChart: {
-              chartType: "COLUMN",
-              legendPosition: "BOTTOM_LEGEND",
-              axis: [
+  // Initialize chart request variables to simplify JSON request body.
+  var axis = [
                 {
                   position: "BOTTOM_AXIS",
                   title: "Vision Labels"
@@ -51,12 +55,9 @@ function createChart(spreadsheetId, authToken) {
                   position: "LEFT_AXIS",
                   title: "Score" 
                 }
-              ],
-              domains: [
-                {
-                  domain: {
-                    sourceRange: {
-                      sources: [
+              ]
+  
+  var domain_sources = [
                         {
                           startRowIndex: 0,
                           endRowIndex: 5,
@@ -64,15 +65,8 @@ function createChart(spreadsheetId, authToken) {
                           endColumnIndex: 1
                         }
                       ]
-                    }
-                  }
-                }
-              ], // closes domains
-              series: [
-                {
-                  series: {
-                    sourceRange: {
-                      sources: [
+  
+  var series_sources = [
                         {
                           startRowIndex: 0,
                           endRowIndex: 7,
@@ -80,14 +74,46 @@ function createChart(spreadsheetId, authToken) {
                           endColumnIndex: 2
                         }
                       ]
+  
+  var domains = [
+                {
+                  domain: {
+                    sourceRange: {
+                      sources: domain_sources
                     }
-                  }, // closes first Series
+                  }
+                }
+              ]
+ 
+  
+  var series = [
+                {
+                  series: {
+                    sourceRange: {
+                      sources: series_sources
+                    }
+                  }, 
                   targetAxis: "LEFT_AXIS"
-                } // closes series item
-              ],
+                } 
+              ]
+  
+  var chart_specs = {
+            title: "Vision API Data",
+            basicChart: {
+              chartType: "COLUMN",
+              legendPosition: "BOTTOM_LEGEND",
+              axis: axis,
+              domains: domains, // closes domains
+              series: series,
               headerCount: 1
             } // closes basicChart
-          }, // closes spec
+          }
+  
+  var requests =  [
+    {
+      addChart: {
+        chart: {
+          spec: chart_specs, // closes spec
           position: {
             newSheet: true
           } // closes position
@@ -95,28 +121,41 @@ function createChart(spreadsheetId, authToken) {
       } // closes add chart
     } // closes requests
   ] // closes requests
+  
+  // Creates full chartRequest variable.
+  var chartRequest = { 
+   requests:requests
   } // closes request
 
-  console.log("Created createChart() request!");
-  
+  // Pushes request to sheet.
   sheet.spreadsheets.batchUpdate({
       spreadsheetId,
       resource: chartRequest,
     }, (err, response) => {
       if (err) {
+        // Handle error
         console.log(err);
       }
-      console.log("createChart response: " + JSON.stringify(response, null, 2));
+      console.log("CreatingChart response is: " + JSON.stringify(response, null, 2));
     });
-} 
+  
+} // closes createChart()
 
-/** Import vision API label detection data into a Google Sheet.
+/**
+ * Writes label detection data to the newly created sheet.
+ * 
+ * @param {String} spreadsheetID ID of sheet to write to.
+ * @param {labels} labels
+ * @param {num} labelScores
+ * @param {String} authToken Authentication to write to user's sheet.
  */
-function writeToSheet(spreadsheetId, labels, labelScores, authToken) {  
+function writeToSheet(spreadsheetId, labels, labelScores, authToken) {
   // Create oauth2Client instance.
   oauth2Client.setCredentials({
    access_token: authToken
   });
+    
+  // Create sheets instance.
   const sheet = google.sheets({version: 'v4', 
                               auth: oauth2Client
                              });
@@ -132,7 +171,7 @@ function writeToSheet(spreadsheetId, labels, labelScores, authToken) {
   labelString = labelString.replace(/[\[\]'\"]+/g,'');
   labelString = labelString.split(',');
 
-  // Push all labels + scores to array.
+  // Print image labels to sheet.
   var values = []
   for (var i = 0; i < dataLength; i++) {
     var dataToPush = [dataString[i], labelString[i]];
@@ -143,26 +182,33 @@ function writeToSheet(spreadsheetId, labels, labelScores, authToken) {
   var body = {
 	values: values
   }
-
+  
   var request = {
     spreadsheetId: spreadsheetId,
     valueInputOption: 'USER_ENTERED',
     range: 'A1:B',
     resource: body,
     auth: oauth2Client
+    // majorDimension = ROWS by default.
   };
     
-  // Push values to sheet.
+  // Update sheet with data.
   sheet.spreadsheets.values.update(request, {
   }).then(function(res) {
-     console.log("Values.update Response: " + JSON.stringify(res, null, 2));
+     console.log("Values updated! Response is: " + JSON.stringify(res, null, 2));
      createChart(spreadsheetId, authToken);
   }).catch(function(err) {
-    console.log("Values.update Error: " + err);
-  }); 
+    console.log("update values err: " + err);
+    //Error is that "requested entity not found"
+  });
 }
 
-/** Update the sheet's title.
+/** 
+ * Sets existing spreadsheet's title accordingly.
+ *
+ * @param {String} spreadsheetID ID of sheet to change.
+ * @param {String} authToken Authentication to change sheet's
+ * properties.
  */
 function setSheetTitle(spreadsheetId, authToken) {
   oauth2Client.setCredentials({
@@ -176,7 +222,7 @@ function setSheetTitle(spreadsheetId, authToken) {
   requests.push({
       updateSpreadsheetProperties: {
         properties: {
-          title: 'Vision API Data',
+          title: 'my new title!',
         },
         fields: 'title',
       },
@@ -188,76 +234,89 @@ function setSheetTitle(spreadsheetId, authToken) {
       resource: batchUpdateRequest,
     }, (err, response) => {
       if (err) {
-        console.log("Update Title Request Error: " + err);
+        // Handle error
+        console.log(err);
       }
-      console.log("Update Title Response: " + response);
+      console.log("BatchUpdate response is: " + response);
     });
 }
 
-/** Create a new Spreadsheet in the Google Account where the 
- * end-user logged in at the client.
+/**
+ * Creates a new spreadsheet in end-user's account.
+ *
+ * @param {String} authToken Authentication to create sheet.
+ * @param {data} labels Information to add to sheet.
+ * @param {data} labelScores Information to add to sheet.
  */
 async function createSheet(authToken, labels, labelScores) {
   oauth2Client.setCredentials({
    access_token: authToken
   });
+  
   const sheet = google.sheets({version: 'v4', 
                               auth: oauth2Client
                              });  
   
+  console.log("Access token is: " + authToken);
+  
   var request = {
     auth: oauth2Client,
   };
-  
-   var url = '';
-   await sheet.spreadsheets.create(request, { 
-  	}).then(function(response)  {
-    var spreadsheetId = response.data.spreadsheetId;
-
-    // Call functions.
-    writeToSheet(spreadsheetId, labels, labelScores, authToken);
-    setSheetTitle(spreadsheetId, authToken);
-    url = response.data.spreadsheetUrl;
-   }).catch(function(err) { 
+   
+   // Create sheet & await response.
+   let response = await sheet.spreadsheets.create(request, { 
+  	}).catch(function(err) { 
      console.log("Caught error is: " + err) 
-   });
-  return url;
+   }); 
+  
+   // Calls additional functions.
+   var spreadsheetId = response.data.spreadsheetId;
+   writeToSheet(spreadsheetId, labels, labelScores, authToken);
+   setSheetTitle(spreadsheetId, authToken);  
+  
+   // Returns url.
+   return response.data.spreadsheetUrl;
 }
 
 
-
-/**Gets label detection labels from the Vision API using
- * raw image bytes.
+/** 
+ * Gets label detection information from Vision API.
+ *
+ * @param {String} imageUrl 
+ * @param {String} authToken Authorization to create Sheet with
+ * obtained Vision API data.
  */
 async function getLabels(imageUrl, authToken) {  
-  // Import the Google Cloud client library.
+  // Imports the Google Cloud client library
   const vision = require('@google-cloud/vision');
-  // Create a client.
+  // Creates a client
   const client = new vision.ImageAnnotatorClient();
+  //console.log("created client");
  
    const imgFileBytes = await getFileBytes(imageUrl);
+   console.log("got file bytes");
    const [result] = await client.labelDetection({image: {content: imgFileBytes}});
    var labelsResult = result.labelAnnotations;
    var labels = labelsResult.map(label => label.description);
    var labelScores = labelsResult.map(label => label.score);
+   console.log("VAR labelScores: " + labelScores);
   
-  // Store the sheet's URL when createSheet() is called.
-  var url = '';
-  await createSheet(authToken, labels, labelScores).then(res => {
-    url = res;
-  });
-  return url;
+   // Returns created sheet's URL.
+   return await createSheet(authToken, labels, labelScores);                                                    
 }
 
-/** Get raw image file bytes.
+/** 
+ * Takes Drive fileID and obtains file bytes.
+ *
+ * @param {String} fileID ID of image Drive file to access/
  */
 async function getFileBytes(fileId) {
-  // Set auth credentials.
+  // get auth
   let auth = await google.auth.getClient({ 
     scopes: ['https://www.googleapis.com/auth/drive']
   });
+  
   let drive = google.drive({version: 'v3', auth});
-
   let response = await drive.files.get({
     fileId: fileId,
     alt: 'media'
@@ -267,6 +326,12 @@ async function getFileBytes(fileId) {
   return Buffer.from(response.data); 
 }
 
+/**
+ * HTTP function that supports CORS requests.
+ *
+ * @param {Object} req Cloud Function request context.
+ * @param {Object} res Cloud Function response context.
+ */
 handleCors = (req, res) => {
   res.set("Access-Control-Allow-Origin", "*");
   res.set("Access-Control-Allow-Methods", "GET");
